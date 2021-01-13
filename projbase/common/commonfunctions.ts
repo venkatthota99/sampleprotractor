a8a8a5d0b3c2135c2a9bec3dbbb0e719c67ff6a8
thota <vthota.ca@gmail.com>
2021-01-13 17:16:38 +0530

    page screenshot commit

--- projbase/common/commonfunctions.ts
+++ projbase/common/commonfunctions.ts
@@ -1,5 +1,14 @@
+import { browser } from "protractor";
 import { ProtractorBase } from "../base/protractorbase";
 
 export class commonfunctions  extends ProtractorBase {
+
+    
+        
+    
+
+
+
+
 
 }
